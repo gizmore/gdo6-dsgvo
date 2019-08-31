e48687e3e0a589409d37864c30860c2f96064808
gizmore <gizmore@wechall.net>
2019-08-31 16:25:53 +0200

    6.10-r42_german_lang

---
+++ lang/dsgvo_de.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+	'dsgvo_info' => 'Diese Website verwendet Cookies. Beim Benutzen dieser Webseite akzeptieren Sie dies. Wir verwenden Cookies ausschliesslich um Ihre Sitzung zu erkennen, und geben keine Informationen an Dritte weiter.', 
+	'btn_accept' => 'Akzeptieren',
+	'msg_cookie_accepted' => 'Sie haben nun Cookies als Vorraussetzung für diese Website akzeptiert.',
+);
--- lang/dsgvo_en.php
+++ lang/dsgvo_en.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'dsgvo_info' => 'This Website uses cookies. By using this site you accept that we use cookies to be able to remember your session while browsing our site',
-	'btn_accept' => 'Accept',
-	'msg_cookie_accepted' => 'You have recognized cookies as a requirement for our services'
+'dsgvo_info' => 'This Website uses cookies.  using this site you accept that we use cookies to be able to remember your session while browsing our site',
+'btn_accept' => 'Accept',
+'msg_cookie_accepted' => 'You have recognized cookies as a requirement for our services'
 );
