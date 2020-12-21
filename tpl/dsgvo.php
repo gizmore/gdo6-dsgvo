a6d34f6c5a790e7cc29821df178036be12e2a37b
gizmore <gizmore@wechall.net>
2020-12-21 18:15:47 +0100

    6.10-r9405_unit_testing

--- tpl/dsgvo.php
+++ tpl/dsgvo.php
@@ -1,7 +1,7 @@
-<div>
+<div id="gdo_dsgvo_panel">
   <div class="gdo_dsgvo_overlay"></div>
   <div class="gdo_dsgvo_info">
-    <?php echo html(t('dsgvo_info')); ?>
-    <a id="gdo-dsgvo-accept-button" href="<?php echo href('DSGVO', 'Accept', '&ref='.urlencode($_SERVER['REQUEST_URI'])); ?>"><?php echo t('btn_accept'); ?></a>
+    <p><?=html(t('dsgvo_info'))?></p>
+    <a id="gdo-dsgvo-accept-button" href="<?=href('DSGVO', 'Accept', '&ref='.urlencode(@$_SERVER['REQUEST_URI']))?>"><?=t('btn_accept')?></a>
   </div>
 </div>
